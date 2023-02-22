68f311344317129f1b7f178e89efeb6d15321976
Michal Stepniewski <michal.stepniewski@adamed.com>
2023-02-22 16:24:25 +0100

    add pepseq.draw.draw_pepseq function  returning png string image of pepseq sequence

--- pepseq/draw.py
+++ pepseq/draw.py
@@ -1,13 +1,9 @@
-import os
-
-
 from pepseq.Drawer import draw_symbols
 from pepseq.get_peptide_json_from_pepseq_format import get_pep_json
 
 
 def draw_pepseq(
     pepseq: str,
-    image_file_name: str = "out.png",
     width: int = 1124,
     height: int = 640,
 ):
@@ -19,14 +15,5 @@ def draw_pepseq(
     symbols = get_pep_json(pepseq)["symbols"]
     symbols[0] = '%s-' % (symbols[0])
     symbols[-1] = '-%s' % (symbols[-1])
-    draw_symbols(symbols, width=width, height=height, out=image_file_name)
-    return
-
-
-def draw_all(pepseq, root_dir='.', width: int = 1124,
-             height: int = 640, image_file_name: str = "out.png"):
-    os.system('mkdir -p %s' % root_dir)
-    pepseq_filename = "%s/%s" % (root_dir, image_file_name)
-
-    draw_pepseq(pepseq, pepseq_filename, width, height)
-    return
+    png_string = draw_symbols(symbols, width=width, height=height)
+    return png_string
