c0f24f87f839a10c04ad5e7d97f27cd1d3fe29fa
Michal Stepniewski <michal.stepniewski@adamed.com>
2023-07-06 13:21:25 +0200

    Modify setup.py to include pepseq/Peptide/database/db.json file

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="pepseq",
     version="1.0",
     description="Module to Read And Write Modified Peptide Repsesentations",
     author="MS",
+    packages=find_packages(),
+    package_data={'':['pepseq/Peptide/database/db.json']},
+    include_package_data=True,
     author_email="omitted_for_now",
     install_requires=["networkx", "rdkit"],
+
 )
