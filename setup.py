7bde09940975aef8b59ceac863ef23bd62a162e1
Michal Stepniewski <michal.stepniewski@adamed.com>
2023-07-06 13:42:40 +0200

    changed package_data_inclusion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="Module to Read And Write Modified Peptide Repsesentations",
     author="MS",
     packages=find_packages(),
-    package_data={'':['pepseq/Peptide/database/db.json']},
+    package_data={'':['pepseq/Peptide/database/db.json','Peptide/database/db.json']},
     include_package_data=True,
     author_email="omitted_for_now",
     install_requires=["networkx", "rdkit"],
